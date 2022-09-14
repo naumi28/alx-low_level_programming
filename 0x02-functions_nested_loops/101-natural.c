070655e0806a67442f0f779ef756d0569bf4fbe5
naumi <naumimuisyo@gmail.com>
2022-09-15 04:02:36 +0800

    13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
   * main - Prints the sum of all multiples of 3 or 5 up to 1024
   *
@@ -20,7 +19,7 @@ int main(void)
 
 		i++;
 	}
-    
+
 	printf("%d\n", z);
 	return (0);
 }
