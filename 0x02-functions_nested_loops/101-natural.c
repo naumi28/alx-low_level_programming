2bdb17eb182ff82320d8aab1bc076de501e0ea99
naumi <naumimuisyo@gmail.com>
2022-09-15 04:08:04 +0800

    13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,19 +4,16 @@
   * main - Prints the sum of all multiples of 3 or 5 up to 1024
   *
   * Return: Always (Success)
-  */ 
-
+  */
 int main(void)
-
 {
 	int i, z = 0;
 
 	while (i < 1024)
-
-    {
+	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-		z += i;
+			z += i;
 		}
 
 		i++;
