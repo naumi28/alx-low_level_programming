3e72df138a233d75b57509ee1a159f77362a1c2f
naumi <naumimuisyo@gmail.com>
2022-09-15 01:28:45 +0800

    1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef _0-putchar
-#define _0-putchar
+#ifndef 0-putchar
+#define 0-putchar
 
 char _putchar(char c);
 void print_alphabet(void);
@@ -15,4 +15,4 @@ int add(int one, int two);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif /* _0-putchar
+#endif /* 0-putchar
