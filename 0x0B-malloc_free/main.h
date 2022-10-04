49dbbab702d37c1c2d9bf9b73006e5e73315f6c7
naumi28 <naumimuisyo@gmail.com>
2022-10-04 12:34:21 -0700

    main

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,15 @@
+#ifndef _MAIN_
+#define _MAIN_
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+void freemem(int **);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
+int get_wordcount(char *str);
+void freememc(char **d);
+
+#endif
